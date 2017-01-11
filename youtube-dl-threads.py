e4b60cd4c3b83e1fd2ac6732bd76e4e663be0bdb
andres7293 <andres7293@gmail.com>
2017-01-11 11:23:10 +0000

    Added option for download file in command line
    Added option for download file with url specified in command line directly. 
    It is not necessary to use a batch file to use this script

--- youtube-dl-threads.py
+++ youtube-dl-threads.py
@@ -1,14 +1,14 @@
 '''
-script de python que usa youtube-dl
-donde cada descargar corre en un hilo
+python script which use youtube-dl where each download runs on a thread
 '''
 
 import os
+import sys
 import argparse
 from threading import Thread
 
 
-def getNumberLines(path):
+def getNumberLines_FromFile(path):
     #gets the numbers of lines that file have
     stream = open(path,mode='r')
     counter = 0
@@ -23,7 +23,7 @@ def getNumberLines(path):
     stream.close()
     return counter
 
-def getUrl_List(path):
+def getUrl_List_FromFile(path):
     # assing each url to a list
     stream = open(path, mode='r')
     counter = 0
@@ -51,21 +51,37 @@ def youtube_dl(url):
     #runs the youtube-dl command
     os.system('youtube-dl '+url)
 
-def main(pathFile):
+def main(pathFile,url_list,file):
 
-    Numberlines = getNumberLines(pathFile)
-    url_list=getUrl_List(pathFile)
-    threads_list = Create_threadList(Numberlines,url_list)
+    if file:
+        #get url from file
+        Numberlines = getNumberLines_FromFile(pathFile)
+        url_list=getUrl_List_FromFile(pathFile)
+        threads_list = Create_threadList(Numberlines,url_list)
+        #Run threads
+        for i in range(Numberlines):
+            threads_list[i].start()
+    else:
+        #calculate numbers of url in list
+        numbers_of_url=len(url_list)
+        threads_list= Create_threadList(numbers_of_url,url_list)
+        #Run threads
+        for i in range(numbers_of_url):
+            threads_list[i].start()
 
-    #Run threads
-    for i in range(Numberlines):
-        threads_list[i].start()
 
 if __name__ == "__main__":
     #python runs from command line
     parser = argparse.ArgumentParser(description='Script which use youtube-dl each download runs on a thread')
-    parser.add_argument('-a', '--file',help='File path', required=True)
+    parser.add_argument('-a', '--file',help='File path', required=False)
+    parser.add_argument('-c', '--url', help='URL List from command line',nargs='+', required=False) #nargs='+' takes one or more arguments
     args=parser.parse_args()
     #get file argument
     pathFile = args.file
-    main(pathFile)
+    cmd_URL_List =args.url
+    if(pathFile):
+        #run youtube-dl threads reading url from file
+        main(pathFile,"",True)
+    if(cmd_URL_List):
+        #run youtube-dl threads using url from command line
+        main("",cmd_URL_List,False)
